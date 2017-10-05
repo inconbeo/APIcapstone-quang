b8e5ecfd04548085d27e0e25d8957bf266278dee
Onesun15 <sunnysharma15@hotmail.com>
2017-10-05 15:35:19 -0400

    got location data and restaurants running

--- main.js
+++ main.js
@@ -8,14 +8,16 @@ const GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
 //const SEARCH_ENDPOINT = 'https://maps.googleapis.com/maps/api/place/nearbysearch/output?parameters';
 
 const STORE = {
+  searchTerm: null,
   latitude: null,
   longitude: null,
-  map: null
+  map: null,
+  keyword: null
 };
 
 
 
-
+// Test Ping to API
 const name = 'richmond';
 $.getJSON(GEOCODE_URL, {
   address: name,
@@ -26,13 +28,46 @@ $.getJSON(GEOCODE_URL, {
 
 
 
+function getSearchLocation(searchValue, callback) {
+  const query = {
+    address: searchValue,
+    key: AUTH_KEY
+  };
+  $.getJSON(GEOCODE_URL, query, callback);
+}
 
 
 
 
 
+function genereateDataList(data) {
+  STORE.latitude = data.results[0].geometry.location.lat;
+  STORE.longitude = data.results[0].geometry.location.lng;
+  const results = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${STORE.latitude},${STORE.longitude}&radius=500&types=restaurant&name=food&key=${AUTH_KEY}`;
+  console.log(results); 
+}
 
+
+
+
+
+function handleSearchClick() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    console.log('hit search');
+    const searchTarget = $(event.currentTarget).find('.js-query');
+    const search = searchTarget.val();
+    STORE.searchTerm = search;
+    searchTarget.val('');
+    getSearchLocation(search, genereateDataList);
+  });
+}
 
 
 //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=YOUR_API_KEY
 //https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=YOUR_API_KEY
+
+
+$(() => {
+  handleSearchClick();
+});
